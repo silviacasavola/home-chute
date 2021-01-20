3d251760edfa46d5f16500abc404535e73194d32
silviacasavola <silcasavola@gmail.com>
2021-01-20 18:34:34 +0100

    updated

--- assets/js/sketch1.js
+++ assets/js/sketch1.js
@@ -1,34 +1,21 @@
-function preload(){
-  // put preload code here
-}
-
-function setup() {
-
-  // put setup code here
-}
-
-function draw() {
-  // put drawing code here
-}
-
 function openAbout() {
-  document.getElementById("about").style.width = "100%";
+  document.getElementById("about-overlay").style.width = "100%";
 }
 
 /* Close when someone clicks on the "x" symbol inside the overlay */
 function closeAbout() {
-  document.getElementById("about").style.width = "0%";
+  document.getElementById("about-overlay").style.width = "0%";
   console.log('chiudi')
 }
 
 function openRan() {
-  document.getElementById("ranking").style.left = "0px";
+  document.getElementById("ranking-overlay").style.left = "0px";
   console.log('non mi vedi')
 }
 
 /* Close when someone clicks on the "x" symbol inside the overlay */
 function closeRan() {
-  document.getElementById("ranking").style.left = "100%";
+  document.getElementById("ranking-overlay").style.left = "100%";
   console.log('chiudimi');
 
 }
